31b8a610ace592e5f1e3b1f8dc67dd3127ad20a6
YSH <sheepsh93@gmail.com>
2021-01-03 17:12:36 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Entity
 @Getter
-@Setter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) //싱글테이블 전략
 @DiscriminatorColumn(name="dtype")
 public abstract class Item {
@@ -22,8 +21,27 @@ public abstract class Item {
 
     private String name;
     private int price;
-    private int stockQuntity;
+    private int stockQuantity;
 
     @ManyToMany(mappedBy="items")
     private List<Category> categories = new ArrayList<>();
+
+
+    //==비즈니스 로직==
+
+    //스톡 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    // sotck 감소
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
 }
