fe264203cf04b12f40395750f7be53e61347c025
YSH <sheepsh93@gmail.com>
2021-01-23 14:32:07 +0900

    간단한 주문조회 V2: 엔티티를 DTO로 변환(최적화x)

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,14 +1,19 @@
 package jpabook.jpashop.api;
 
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 //Order와 Member는 ManyToOne 관계
@@ -53,4 +58,38 @@ public class OrderSimpleApiController {
         }
         return all;
     }
+
+    //dto를 이용해보자
+    //이렇게 해서 돌리면 쿼리가 5개가 날아간다. => N+1발생
+    //기본적으로 Order를 한 번 조회하면 관련된 delivery와 member 각각 하나씩 총 두 개의 쿼리가 나가는데 현재 orders에는 두 건이 존재한다.
+    //그래서 결과적으로 처음 Order를 가져오는데 1번,그 내부적으로 delivery와 Member에 해당하는 각각 2개씩 해서 총 다섯 개의 쿼리가 나가는 것이다.
+    // Order 2개
+    // 1+ 회원 N + 배송 N
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getMember().getAddress();
+        }
+    }
+
 }
